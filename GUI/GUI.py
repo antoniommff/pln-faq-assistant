882ff0a0998ad61cdc5fa0059d130ce72092e7e3
nicrodrui <nicolasrodriguezruiz.mat@gmail.com>
2026-05-12 16:29:34 +0200

    Ahora cargamos el modelo en segundo plano

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,13 +1,21 @@
 import flet as ft
+import asyncio
 from auxiliar.entidades import EntityExtractor
 
 
+def load():
+    extractor = EntityExtractor()
+    extractor.add_vectors()
+    return extractor
 
-def main(page: ft.Page):
+
+async def main(page: ft.Page):
+
     extractor = None
-    # 1. Configuración de la página (Ventana)
+
+    # Configuración
     page.title = "FAQs"
-    page.theme_mode = ft.ThemeMode.DARK # Modo oscuro automático
+    page.theme_mode = ft.ThemeMode.DARK
     page.window_width = 450
     page.window_height = 400
     page.window_resizable = False
@@ -15,23 +23,7 @@ def main(page: ft.Page):
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.padding = 40
 
-    # 2. Lógica de la aplicación
-    def enviar_click(e):
-        if extractor is None:
-            texto_salida.value = "El modelo aún se está cargando..." # MUY IMPROBABLE QUE PASE LOL
-            texto_salida.color = ft.Colors.ORANGE_400
-            return
-        if not campo_entrada.value:
-            texto_salida.value = "Por favor, escribe algo primero."
-            texto_salida.color = ft.Colors.RED_400
-        else:
-            items, _ = extractor.extract(campo_entrada.value.rstrip("\r").rstrip("\n"))
-            texto_salida.value = f"Se han extraido las siguientes entidades: {items}"
-            texto_salida.color = ft.Colors.GREEN_400
-
-        page.update() # Refresca la interfaz para mostrar los cambios
-
-    # 3. Componentes de la interfaz (Widgets)
+    # Elementos de la app
     titulo = ft.Text(
         "Bienvenido",
         size=30,
@@ -41,51 +33,59 @@ def main(page: ft.Page):
 
     campo_entrada = ft.TextField(
         label="Escribe algo aquí...",
-        border_radius=15,
-        border_color=ft.Colors.BLUE_700,
-        focused_border_color=ft.Colors.BLUE_400,
         width=300,
-        text_align=ft.TextAlign.LEFT,
-    )
-
-    boton_enviar = ft.Button(
-        content=ft.Text("Enviar datos"),
-        style=ft.ButtonStyle(
-            color=ft.Colors.WHITE,
-            bgcolor=ft.Colors.BLUE_700,
-            shape=ft.RoundedRectangleBorder(radius=10),
-        ),
-        on_click=enviar_click
+        border_radius=15,
     )
 
     texto_salida = ft.Text(
-        value="Esperando entrada...",
-        size=16,
-        italic=True,
-        color=ft.Colors.GREY_500
+        value="Cargando motor IA...",
+        color=ft.Colors.ORANGE_400
     )
+    # Logica de la app
+    async def enviar_click(e):
+        nonlocal extractor
 
-    # 4. Agregar componentes a la página
+        if extractor is None:
+            texto_salida.value = "El modelo sigue cargando..."
+            texto_salida.color = ft.Colors.ORANGE_400
+
+        elif not campo_entrada.value:
+            texto_salida.value = "Escribe algo primero."
+            texto_salida.color = ft.Colors.RED_400
+
+        else:
+            items, _ = extractor.extract(
+                campo_entrada.value.rstrip("\r\n")
+            )
+
+            texto_salida.value = f"Entidades:\n{items}"
+            texto_salida.color = ft.Colors.GREEN_400
+
+        page.update()
+
+    boton = ft.ElevatedButton(
+        "Enviar",
+        on_click=enviar_click
+    )
+    # Construccion de la app
     page.add(
         titulo,
-        ft.Divider(height=20, color="transparent"),
         campo_entrada,
-        ft.Divider(height=10, color="transparent"),
-        boton_enviar,
-        ft.Divider(height=30, color="transparent"),
+        boton,
         texto_salida
     )
 
-    # Muestra un mensajito mientras carga # No funciona, quizas un poco de multithreading molaria
-    texto_salida.value = "Cargando motor de IA..."
     page.update()
 
-    # Carga el modelo
-    extractor = EntityExtractor()
-    extractor.add_vectors()
+    # =========================
+    # CARGA EN SEGUNDO PLANO
+    # =========================
 
-    # Avisa que ya terminó
-    texto_salida.value = "Esperando entrada..."
+    extractor = await asyncio.to_thread(load)
+
+    texto_salida.value = "Modelo cargado correctamente."
+    texto_salida.color = ft.Colors.GREEN_400
+
     page.update()
 
 
