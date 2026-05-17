eac273bc1cb7029b202ab041f6bfe5e188863276
nicolasrodriguezruiz <nicolasrodriguezruiz.mat@gmail.com>
2026-05-17 18:34:36 +0200

    Cambios estéticos y modo blaco

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -27,7 +27,7 @@ async def main(page: ft.Page):
     page.update()
 
     # Lógica de la app
-    async def ir_a_github(e):
+    async def go_to_github(e):
         await page.launch_url("https://github.com/antoniommff/pln-faq-assistant")
 
     async def enviar_click(e):
@@ -38,63 +38,83 @@ async def main(page: ft.Page):
         nonlocal intention_models
 
         if extractor is None or detector is None or preprocesor is None:
-            texto_salida.value = "El modelo sigue cargando..."
-            texto_salida.color = ft.Colors.ORANGE_400
+            output_text.value = "El modelo sigue cargando..."
+            output_text.color = ft.Colors.ORANGE_400
 
-        elif not campo_entrada.value:
-            texto_salida.value = "Escribe algo primero."
-            texto_salida.color = ft.Colors.RED_400
+        elif not input_field.value:
+            output_text.value = "Escribe algo primero."
+            output_text.color = ft.Colors.RED_400
 
         else:
-            text = campo_entrada.value.rstrip("\r\n")
+            text = input_field.value.rstrip("\r\n")
             lang = predict_language(text, detector)
 
-            clean_text = preprocesor.process_text(text=text, lang=lang, is_predict=True)
+            clean_text = preprocesor.process_text(text=text, lang=lang, is_predict=False)
 
             items, entities = extractor.extract(
                 clean_text=clean_text,
                 lang=lang,
             )
+
             vector = vectorizers[lang].transform([clean_text])
             intent_num = int(intention_models[lang].predict(vector)[0])
             intent_name = REV_INTENT.get(intent_num, 'unknown')
 
-            display = "\n" + prettify(entities) if boton_superior.content == "Categorías" else items
-            texto_salida.value = f"Idioma (T1): {lang.upper()}\n\nIntención (T2): {intent_name}\n\nEntidades (T3): {display}"
-            texto_salida.color = ft.Colors.GREEN_400
+            display = "\n" + prettify(entities) if upper_button.content == "Categorías" else items
+            output_text.value = f"Idioma (T1): {lang.upper()}\n\nIntención (T2): {intent_name}\n\nEntidades (T3): {display}"
+            output_text.color = ft.Colors.GREEN_400
 
         page.update()
 
-    async def cambiar_texto_boton(e):
-        boton_superior.content = "Lista" if boton_superior.content == "Categorías" else "Categorías"
+    async def change_text_button(e):
+        upper_button.content = "Lista" if upper_button.content == "Categorías" else "Categorías"
         page.update()
 
-    async def rotar_titulo():
-        nuevo_idx = idx_mensaje
-        while nuevo_idx == idx_mensaje:
-            nuevo_idx = random.randint(0, len(mensajes) - 1)
+    async def rotate_title():
+        new_idx = idx_message
+        while new_idx == idx_message:
+            new_idx = random.randint(0, len(messages) - 1)
 
         if random.randint(0, 1000) == 17:
-            titulo_texto.value = "¡Literalmente 1 entre 1000!"
+            title_text.value = "¡Literalmente 1 entre 1000!"
 
-        titulo_texto.value = mensajes[nuevo_idx]
-        titulo_animado.content = ft.Text(
-            titulo_texto.value,
+        title_text.value = messages[new_idx]
+        animated_title.content = ft.Text(
+            title_text.value,
             size=30,
             weight=ft.FontWeight.BOLD,
             color=ft.Colors.BLUE_200,
         )
         page.update()
 
-    boton_superior = ft.Button(
-        content="Lista",
-        on_click=cambiar_texto_boton,
-    )
+    async def chage_theme(e):
+        theme_switch.thumb_icon = (
+            ft.Icons.DARK_MODE if theme_switch.value else ft.Icons.LIGHT_MODE
+        )
+
+        if theme_switch.value:
+            page.theme_mode = ft.ThemeMode.LIGHT
+        else:
+            page.theme_mode = ft.ThemeMode.DARK
+
+        page.update()
+
 
     # Elementos de la app
 
+    upper_button = ft.Button(
+        content="Lista",
+        on_click=change_text_button,
+    )
+
+    theme_switch = ft.Switch(
+        value=False,
+        thumb_icon=ft.Icons.LIGHT_MODE,
+        on_change=chage_theme
+    )
+
     # Lista de mensajes para el ciclo
-    mensajes = [
+    messages = [
         "Bienvenido", "Welcome", "Ask anything!", "Consulta tus dudas",
         "FAQ's", "¡Escribe algo!", "Comida comida comida", "Now with 20% less cyanide",
         "Condimentando tus dudas", "Sin grumos en la información",
@@ -102,68 +122,92 @@ async def main(page: ft.Page):
         "La receta del éxito (y del flan)", "From (data) farm to screen",
         "Don't let your soup get cold!", "Gordon-Ramsay-approved (maybe)",
     ]
-    idx_mensaje = 0
+    idx_message = 0
 
-    titulo_texto = ft.Text(
-        mensajes[idx_mensaje],
+    title_text = ft.Text(
+        messages[idx_message],
         size=30,
         weight=ft.FontWeight.BOLD,
         color=ft.Colors.BLUE_200,
     )
 
     # Componente que gestiona la animación de deslizamiento
-    titulo_animado = ft.AnimatedSwitcher(
-        titulo_texto,
+    animated_title = ft.AnimatedSwitcher(
+        title_text,
         transition=ft.AnimatedSwitcherTransition.FADE,  # Desliza hacia la derecha
         duration=500,  # Duración de la animación en ms
         reverse_duration=500,
         switch_in_curve=ft.AnimationCurve.EASE_OUT,
     )
 
-    campo_entrada = ft.TextField(
+    input_field = ft.TextField(
         label="Como hacer pollo al horno",
-        width=300,
+        width=400,
         border_radius=15,
     )
-    campo_entrada.on_submit = enviar_click
+    input_field.on_submit = enviar_click
 
-    texto_salida = ft.Text(
+    output_text = ft.Text(
         value="Cargando motor IA...",
         color=ft.Colors.ORANGE_400,
     )
 
-    boton = ft.Button(
+    button = ft.Button(
         "Enviar",
         on_click=enviar_click,
     )
 
     # Construcción de la app
     page.add(
-        # Fila superior para el botón izquierdo
-        ft.Row(
+        ft.Stack(
+            expand=True,
             controls=[
-                boton_superior,
-                ft.IconButton(
-                    icon=ft.Icons.CODE,
-                    tooltip="Ver en GitHub",
-                    on_click=ir_a_github,
+                # =========================
+                # CONTENIDO PRINCIPAL
+                # =========================
+                ft.Column(
+                    controls=[
+                        # Fila superior
+                        ft.Row(
+                            controls=[
+                                upper_button,
+                                ft.IconButton(
+                                    icon=ft.Icons.CODE,
+                                    tooltip="Ver en GitHub",
+                                    on_click=go_to_github,
+                                ),
+                            ],
+                            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                        ),
+
+                        # Contenido central
+                        ft.Column(
+                            controls=[
+                                animated_title,
+                                ft.Container(height=20),
+                                input_field,
+                                button,
+                                output_text,
+                                ft.Container(expand=True),
+                            ],
+                            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                            alignment=ft.MainAxisAlignment.CENTER,
+                            expand=True,
+                        ),
+                    ],
+                    expand=True,
+                ),
+
+                # =========================
+                # SWITCH ABAJO DERECHA
+                # =========================
+                ft.Container(
+                    content=theme_switch,
+                    alignment=ft.Alignment(1, 1),
+                    padding=20,
                 ),
             ],
-            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
-        ),
-        # Contenedor central para el resto de elementos
-        ft.Column(
-            controls=[
-                titulo_animado,
-                campo_entrada,
-                boton,
-                texto_salida,
-                ft.Container(expand=True),
-            ],
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-            alignment=ft.MainAxisAlignment.CENTER,
-            expand=True,  # Asegura que el contenido se mantenga centrado
-        ),
+        )
     )
 
     page.update()
@@ -174,15 +218,15 @@ async def main(page: ft.Page):
 
     extractor, detector, preprocesor, vectorizers, intention_models = await asyncio.to_thread(load)
 
-    texto_salida.value = "Modelo cargado correctamente."
-    texto_salida.color = ft.Colors.GREEN_400
+    output_text.value = "Modelo cargado correctamente."
+    output_text.color = ft.Colors.GREEN_400
 
     page.update()
 
     # Bucle infinito para la rotación cada 15 segundos
     while True:
         await asyncio.sleep(15)
-        await rotar_titulo()
+        await rotate_title()
 
 
 if __name__ == "__main__":
